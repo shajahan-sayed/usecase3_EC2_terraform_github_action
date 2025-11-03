dca434bdcb631a95cb92671790cd028e991749b6
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-03 20:07:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "gitaction_ec2" {
  ami = var.ami_id
  instance_type = var.instance_type
  key_name = var.key_name
- subnet_id = var.gitaction_subnet.id
+ subnet_id = aws_subnet.gitaction_subnet.id
  security_groups = [aws_security_group.gitaction_sg.id]
 }
