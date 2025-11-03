ae8f09bc2c523ff18102adaf59d1eb644148538d
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-03 18:12:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,78 @@
+#creating vpc
+resource "aws_vpc" "vpc_github_action" {
+ cidr_block = var.vpc_github_action
+
+ tags = {
+  Name = "vpc_github_action"
+ }
+}
+
+ #creating subnet
+
+ resource "aws_subnet" "gitaction_subnet" {
+  vpc_id = onaws_vpc.vpc_github_action
+  cidr_block = var.gitaction_subnet.id
+
+  tags = { 
+   Name = "gitaction_subnet"
+  }
+}
+
+  #creating internet gateway
+  resource "aws_internet_gateway" "gitaction_igw"{
+   vpc_id = aws_vpc.vpc_github_action
+
+   tags = {
+    Name = "gitaction_igw"
+   }
+  }
+
+   #creating route table 
+   resource "aws_route_table" "gitaction_route_table"{
+    vpc.id = vpc_id = onaws_vpc.vpc_github_action
+
+    tags = {
+     Name = "gitaction_route_table"
+    }
+   }
+   #creating route to attach internet gateway with route table
+    resource "aws_route" "gitaction_route" {
+     gateway_id = aws_internet_gateway.gitaction_igw.id
+     subnet_id = aws_subnet.gitaction_subnet.id
+     destination_cidr_block = "0.0.0.0/0"
+    }
+
+    #creating route table association 
+    resource "aws_route_table_association" "gitaction_route_association" {
+     route_table_id = aws_route_table.gitaction_route.id
+     subnet_id = aws_subnet.gitaction_subnet.id
+    }
+
+    #creating security group
+    resource "aws_security_group" "gitaction_sg" {
+     description = "allowing ports http (80) and ssh(22)"
+
+     ingress {
+      description = "allowing ssh port"
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+     }
+     ingress {
+      description = "allowing ssh port"
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+     }
+     egress {
+      description = "allow all outbound"
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_block = ["0.0.0.0/0"]
+     }
+
+     
+    
